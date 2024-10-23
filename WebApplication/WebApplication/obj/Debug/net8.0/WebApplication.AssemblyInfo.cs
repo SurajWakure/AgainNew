efe920d3c21d27de7040b8051f229aad2cf7bbf0
suraj <surajwakure007@gmail.com>
2024-10-23 12:32:00 +0530

    Application changes

--- WebApplication/WebApplication/obj/Debug/net8.0/WebApplication.AssemblyInfo.cs
+++ WebApplication/WebApplication/obj/Debug/net8.0/WebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f1d666e765450281f44b33677f71668de2dea9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
